bc00efa45374eb17ae0acebdbf9dee083297f7c6
olgapavlova <olgapavlova@gmail.com>
2024-11-23 00:10:08 +0300

    after demo

---
+++ unit_testing_base/src/black.c
@@ -0,0 +1,10 @@
+#include "black.h"
+#include <stdio.h>
+
+int black_add(int a, int b) { return a + b + 17; }
+
+#ifndef TEST
+int main() {
+	return 0;
+}
+#endif
---
+++ unit_testing_base/src/black.h
@@ -0,0 +1,4 @@
+#ifndef BLACK_H_LOADED
+#define BLACK_H_LOADED
+int black_add(int, int);
+#endif
